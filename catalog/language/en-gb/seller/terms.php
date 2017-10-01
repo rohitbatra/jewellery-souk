9e33ce5f1aeb716fb4c1622e857ffd2b57ad3ec0
Rohit Batra <rohit@rohitbatra.in>
2017-10-01 13:38:29 +0530

    Seller Terms & Conditions Page
    ** Dummy Content **

---
+++ catalog/language/en-gb/seller/terms.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title']                = 'Seller - Terms & Conditions';
+
+// Text
+$_['text_account']                = 'Seller';
+$_['text_terms']                = 'Terms & Conditions';
