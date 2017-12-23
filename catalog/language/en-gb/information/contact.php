8af753f8f898ce6cec4568a8aea741e0c434a613
Rohit Batra <rohit@rohitbatra.in>
2017-12-23 16:28:22 +0530

    Storefront Contact Us Page - REFACTORED

--- catalog/language/en-gb/information/contact.php
+++ catalog/language/en-gb/information/contact.php
@@ -1,27 +1,15 @@
 <?php
 // Heading
-$_['heading_title']  = 'Contact Us';
+$_['heading_title']                = 'Contact Us';
 
 // Text
-$_['text_location']  = 'Our Location';
-$_['text_store']     = 'Our Stores';
-$_['text_contact']   = 'Contact Form';
-$_['text_address']   = 'Address';
-$_['text_telephone'] = 'Telephone';
-$_['text_fax']       = 'Fax';
-$_['text_open']      = 'Opening Times';
-$_['text_comment']   = 'Comments';
-$_['text_success']   = '<p>Your enquiry has been successfully sent to the store owner!</p>';
+$_['text_contact']                = 'Contact Us';
 
-// Entry
-$_['entry_name']     = 'Your Name';
-$_['entry_email']    = 'E-Mail Address';
-$_['entry_enquiry']  = 'Enquiry';
+// Entries
+$_['entry_name']                = 'Full Name';
+$_['entry_number']                = 'Contact Number';
+$_['entry_email']                = 'E-Mail';
+$_['entry_query']                = 'Query';
 
-// Email
-$_['email_subject']  = 'Enquiry %s';
-
-// Errors
-$_['error_name']     = 'Name must be between 3 and 32 characters!';
-$_['error_email']    = 'E-Mail Address does not appear to be valid!';
-$_['error_enquiry']  = 'Enquiry must be between 10 and 3000 characters!';
+// Buttons
+$_['button_send']                = 'Send';
