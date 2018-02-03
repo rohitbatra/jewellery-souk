066834e0fe7260434f527062de1397322cbfff5f
Rohit Batra <rohit@rohitbatra.in>
2018-02-03 15:01:41 +0530

    Added language to page

--- catalog/language/en-gb/seller/payment_process.php
+++ catalog/language/en-gb/seller/payment_process.php
@@ -10,7 +10,7 @@ $_['text_payment_process']       = 'Payment';
 $_['text_seller_info'] = 'Seller Info:';
 $_['text_payment_info'] = 'Payment Info:';
 $_['text_welcome'] = 'Welcome <strong> %s </strong>, <br/>';
-$_['text_message']       = 'Your Seller account has been registered successfully! <br/> Please click <strong>Continue</strong> button below to Pay your Seller Registration Fees: <strong> %s </strong> <br/> We will redirect you to CCAvenue Payment Gateway, to complete your Payment';
+$_['text_message']       = 'Your Seller account has been registered successfully! <br/> Please click <strong>Continue</strong> button below to Pay your Seller Registration Fees: <strong> %s </strong> <br/> If you\'ve any doubts/queries please feel free to contact us at <a href="mailto:seller.support@sezplus.com">seller.support@sezplus.com</a>, our team will also get in-touch with you in the next 24 Hours. <br/> We will redirect you to CCAvenue Payment Gateway, to complete your Payment.';
 
 // Error
 $_['error_custom_field']          = '%s required!';
