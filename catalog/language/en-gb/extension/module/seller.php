06ab81240efdcb31326bdc2469ff02d7c02cbec4
Rohit Batra <rohit@rohitbatra.in>
2018-12-30 22:34:36 +0530

    Seller Module -> Right Column language changes.

--- catalog/language/en-gb/extension/module/seller.php
+++ catalog/language/en-gb/extension/module/seller.php
@@ -1,12 +1,12 @@
 <?php
 // Heading
-$_['heading_title']    = 'Seller';
+$_['heading_title']     = 'Seller';
 
 // Text
-$_['text_register']    = 'Register';
-$_['text_login']       = 'Login';
-$_['text_forgotten']   = 'Forgotten Password';
-$_['text_faqs']   = 'Frequently Asked Questions!';
-$_['text_terms']   = 'Terms & Conditions';
+$_['text_register']     = 'Seller Register';
+$_['text_login']        = 'Seller Login';
+$_['text_forgotten']    = 'Seller Forgotten Password';
+$_['text_faqs']         = 'Frequently Asked Questions!';
+$_['text_terms']        = 'Terms & Conditions';
 $_['text_contact_us']   = 'Contact Us';
-$_['text_pricing']   = 'Pricing';
+$_['text_pricing']      = 'Pricing';
